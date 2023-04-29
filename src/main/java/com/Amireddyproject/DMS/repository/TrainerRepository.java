6625b293143b33a2680405fb6c62f06d7150ef25
Amireddy1 <amireddy.shruthi@gmail.com>
2023-04-29 15:40:18 -0500

    first commit

---
+++ src/main/java/com/Amireddyproject/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.Amireddyproject.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.Amireddyproject.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
---
+++ src/test/java/com/Amireddyproject/DMS/DogsManagementSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Amireddyproject.DMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsManagementSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
